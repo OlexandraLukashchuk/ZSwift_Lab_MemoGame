13a7f59550cc0c279cfb0960341189bb9402a8b6
Oleksandra Lukashchuk <83826206+OlexandraLukashchuk@users.noreply.github.com>
2024-12-03 11:41:50 +0100

    Update Cardview.swift

--- MemoGame_ZSwift/Cardview.swift
+++ MemoGame_ZSwift/Cardview.swift
@@ -14,7 +14,14 @@ struct CardView: View {
     
     var body: some View {
         CirclePart(endAngel: .degrees(270))
-            .overlay(Text(card.content))
+            .overlay(Text(card.content).font(.system(size: Constrants.FontSize.large))
+                     .minimumScaleFactor()
+                     .multilineTextAligment(.center)
+                     .padding(Constrants.inserts)
+                     
+                     .rotationEffect(.degrees(card.isMatched? 360:0))
+                     .animation(.spin(duration:2, value: isMatched ? 0:1))
+                     
             .padding()
             .modifier(TransformIntoCard(isFaceUp: card.isFacedUp))
             
